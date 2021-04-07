b5cdd5cdca9ec8a0923bc75759c8362b2e7e01bc
kalmansour <khaledalmansour24@gmail.com>
2021-04-07 11:29:18 +0300

    overlay tab

--- gumroad/src/components/Overlay.js
+++ gumroad/src/components/Overlay.js
@@ -1,78 +1,19 @@
 import React from "react";
-// import { Tabs, Tab } from "react-bootstrap";
+import { Tabs, Tab } from "react-bootstrap";
 
 const Overlay = () => {
   return (
-    <div>
-      <nav>
-        <div class="nav nav-tabs" id="nav-tab" role="tablist">
-          <button
-            class="nav-link active"
-            id="nav-home-tab"
-            data-bs-toggle="tab"
-            data-bs-target="#nav-home"
-            type="button"
-            role="tab"
-            aria-controls="nav-home"
-            aria-selected="true"
-          >
-            Home
-          </button>
-          <button
-            class="nav-link"
-            id="nav-profile-tab"
-            data-bs-toggle="tab"
-            data-bs-target="#nav-profile"
-            type="button"
-            role="tab"
-            aria-controls="nav-profile"
-            aria-selected="false"
-          >
-            Profile
-          </button>
-          <button
-            class="nav-link"
-            id="nav-contact-tab"
-            data-bs-toggle="tab"
-            data-bs-target="#nav-contact"
-            type="button"
-            role="tab"
-            aria-controls="nav-contact"
-            aria-selected="false"
-          >
-            Contact
-          </button>
-        </div>
-      </nav>
-      <div class="tab-content" id="nav-tabContent">
-        <div
-          class="tab-pane fade show active"
-          id="nav-home"
-          role="tabpanel"
-          aria-labelledby="nav-home-tab"
-        >
-          <div>
-            <h1>Supp</h1>
-            <p>We will be adding overlay content here</p>
+    <div style={{ marginLeft: 200, marginRight: 200 }}>
+      <Tabs defaultActiveKey="overlay" id="uncontrolled-tab-example">
+        <Tab eventKey="overlay" title="Overlay">
+          <h6>Overlay Content</h6>
+        </Tab>
+        <Tab eventKey="embed" title="Embed">
+          <div style={{ marginTop: 20 }}>
+            <p>Embed Content Here</p>
           </div>
-        </div>
-        <div
-          class="tab-pane fade"
-          id="nav-profile"
-          role="tabpanel"
-          aria-labelledby="nav-profile-tab"
-        >
-          ...
-        </div>
-        <div
-          class="tab-pane fade"
-          id="nav-contact"
-          role="tabpanel"
-          aria-labelledby="nav-contact-tab"
-        >
-          ...
-        </div>
-      </div>
+        </Tab>
+      </Tabs>
     </div>
   );
 };
