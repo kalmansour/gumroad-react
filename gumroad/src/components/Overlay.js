1e2f730d89670d0b200a02cd4739cbdc34e4a491
kalmansour <khaledalmansour24@gmail.com>
2021-04-08 19:40:11 +0300

    overlay

--- gumroad/src/components/Overlay.js
+++ gumroad/src/components/Overlay.js
@@ -5,7 +5,7 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 import { BsClipboard } from "react-icons/bs";
 import { CgCopy } from "react-icons/cg";
 import styled from "styled-components";
-// import { ImEmbed2 } from "react-icons/im";
+import { ImEmbed2 } from "react-icons/im";
 
 const Overlay = () => {
   const [product, setProduct] = useState("Buy my product");
@@ -41,26 +41,28 @@ const Overlay = () => {
     >
       <span className="fa fa-list-alt fa-fw"></span>
       <CgCopy size="5em" />
-      <h5>Overlay</h5>
-      <p>Inline purchases without leaving your site</p>
+      <h5 style={{ color: "darkslategray" }}>Overlay</h5>
+      <p style={{ color: "#a2a2a2" }}>
+        Inline purchases without leaving your site
+      </p>
     </div>
   );
 
-  // const embedTitle = (
-  //   <div
-  //     style={{
-  //       display: "flex",
-  //       flexDirection: "column",
-  //       alignItems: "center",
-  //       textAlign: "center",
-  //     }}
-  //   >
-  //     <span className="fa fa-list-alt fa-fw"></span>
-  //     <ImEmbed2 size="5em" />
-  //     <h5>Embed</h5>
-  //     <p>Inline purchases without leaving your site</p>
-  //   </div>
-  // );
+  const embedTitle = (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        textAlign: "center",
+      }}
+    >
+      <span className="fa fa-list-alt fa-fw"></span>
+      <ImEmbed2 size="5em" />
+      <h5>Embed</h5>
+      <p>Inline purchases without leaving your site</p>
+    </div>
+  );
 
   return (
     <Container>
@@ -76,7 +78,10 @@ const Overlay = () => {
                 className="form-control"
                 onChange={handleChange}
               />
-              <div class="form-check" style={{ margin: "15px 20px" }}>
+              <div
+                class="form-check"
+                style={{ margin: "15px 20px", color: "#a2a2a2" }}
+              >
                 <input
                   class="form-check-input"
                   value={payment}
@@ -88,7 +93,10 @@ const Overlay = () => {
                   Auto-trigger the payment form
                 </label>
               </div>
-              <div class="form-check" style={{ margin: "15px 20px" }}>
+              <div
+                class="form-check"
+                style={{ margin: "15px 20px", color: "#a2a2a2" }}
+              >
                 <input
                   class="form-check-input"
                   type="checkbox"
@@ -101,63 +109,52 @@ const Overlay = () => {
                 </label>
               </div>
             </ProductBox>
-            <ClipboardBox>
-              <div
-                style={{
-                  textAlign: "center",
-                  alignItems: "center",
-                  display: "flex",
-                  flexDirection: "column",
-                }}
-              >
-                <input
-                  type="text"
-                  class="btn btn-light"
-                  placeholder="Buy my product"
-                  value={product}
-                  style={{
-                    margin: 50,
-                    boxShadow: "0 5px 15px rgba(0, 0, 0, 0.05)",
-                    border: "solid",
-                    borderWidth: "1px",
-                    borderColor: "grey",
-                    justifyContent: "center",
-                  }}
-                  readonly="readonly"
-                />
-                <h6>Copy and paste this code into your website</h6>
-                <h8>
-                  We highly recommend you have an SSL certificate to increase
-                  buyer confidence.
-                </h8>
-                <div
-                  class="demo border"
-                  style={{ backgroundColor: "white", width: 500, height: 100 }}
+            <ClipboardContainer>
+              <ProductPreview
+                type="text"
+                placeholder="Buy my product"
+                value={product}
+                readonly="readonly"
+              />
+              <DirectionsText>
+                Copy and paste this code into your website
+              </DirectionsText>
+              <RecommendedText>
+                We highly recommend you have an SSL certificate to increase
+                buyer confidence.
+              </RecommendedText>
+              <CodeBox>
+                <OverlayCode placeholder={code} readonly="readonly" />
+                <CopyToClipboard
+                  text={code}
+                  onCopy={() => setCopy({ copy: true })}
                 >
-                  <textarea
-                    placeholder={code}
-                    style={{ width: 500, height: 80, resize: "none" }}
-                    readonly="readonly"
+                  <BsClipboard
+                    size="2em"
+                    style={{
+                      marginTop: "auto",
+                      marginBottom: "auto",
+                      marginRight: "10px",
+                      color: "turquoise",
+                    }}
                   />
-                  <CopyToClipboard
-                    text={code}
-                    onCopy={() => setCopy({ copy: true })}
-                  >
-                    <BsClipboard class="float-right" size="2em" />
-                  </CopyToClipboard>
-                  {copy ? <span style={{ color: "red" }}>Copied.</span> : null}
-                </div>
-              </div>
-            </ClipboardBox>
+                </CopyToClipboard>
+                {copy ? (
+                  <span style={{ color: "tomato", margin: "5px 5px" }}>
+                    Copied!
+                  </span>
+                ) : null}
+              </CodeBox>
+            </ClipboardContainer>
           </Tab.Content>
         </Tab>
-        {/* <Tab eventKey="embed" title={embedTitle}>
+        <Tab eventKey="embed" title={embedTitle}>
           <Tab.Content>
             <div class="demo border" style={{ height: 400 }}>
               <p>Embed Content Here</p>
             </div>
           </Tab.Content>
-        </Tab> */}
+        </Tab>
       </Tabs>
     </Container>
   );
@@ -170,6 +167,7 @@ const Container = styled.div`
   margin-right: 100px;
   margin-bottom: 50px;
   width: 700px;
+  border-radius: 5px;
 `;
 
 const ProductBox = styled.div`
@@ -177,20 +175,71 @@ const ProductBox = styled.div`
   border: solid #e1e1e1;
   border-width: 0px 1px;
   height: 160px;
+  border-top-right-radius: 1px;
 `;
 
 const ButtonTitle = styled.h6`
   margin-left: 20px;
+  color: #858585;
 `;
 
 const InputProductText = styled.input`
   margin-left: 20px;
   width: 90%;
+  color: #a2a2a2;
 `;
 
-const ClipboardBox = styled.div`
+const ClipboardContainer = styled.div`
   border: solid #e1e1e1;
   border-width: 1px 1px;
   height: 350px;
   background-color: #f5f5f5;
+  text-align: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+`;
+
+const ProductPreview = styled.input`
+  margin: 50px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.07);
+  border: solid;
+  border-color: lightgrey;
+  border-width: 1px;
+  justify-content: center;
+  text-align: center;
+  height: 50px;
+  border-radius: 5px;
+  font-weight: bolder;
+  color: #a2a2a2;
+`;
+
+const DirectionsText = styled.h6`
+  color: darkslategray;
+`;
+
+const RecommendedText = styled.p`
+  color: #858585;
+  font-size: 12px;
+`;
+
+const CodeBox = styled.div`
+  background-color: white;
+  width: 500px;
+  height: 100px;
+  display: flex;
+  flex-direction: row;
+  border: solid #e1e1e1;
+  border-width: 1px 1px;
+  border-radius: 5px;
+`;
+
+const OverlayCode = styled.textarea`
+  width: 500px;
+  height: 80px;
+  resize: none;
+  border-width: 0px;
+  margin: 10px;
 `;
