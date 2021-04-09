f0170c48f84d83aea68a73f99d27946641966c53
kalmansour <khaledalmansour24@gmail.com>
2021-04-09 12:18:40 +0300

    embed

--- gumroad/src/components/widget/Widget.js
+++ gumroad/src/components/widget/Widget.js
@@ -4,7 +4,7 @@ import { Tabs, Tab } from "react-bootstrap";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { BsClipboard } from "react-icons/bs";
 import { CgCopy } from "react-icons/cg";
-// import styled from "styled-components";
+import styled from "styled-components";
 import { ImEmbed2 } from "react-icons/im";
 
 // Styles
@@ -25,10 +25,15 @@ const Widget = () => {
   const [product, setProduct] = useState("Buy my product");
   const [payment, setPayment] = useState("");
   const [singleProduct, setSingleProduct] = useState("");
+  const [embedProduct, setEmbedProduct] = useState("");
+
   const [copy, setCopy] = useState(false);
 
-  const code = `<script src="https://gumroad.com/js/gumroad.js"></script>
+  const overlayCode = `<script src="https://gumroad.com/js/gumroad.js"></script>
   <a class="gumroad-button" href="https://gum.co/demo${payment}" ${singleProduct}>${product}</a>`;
+
+  const embedCode = `<script src="https://gumroad.com/js/gumroad-embed.js"></script>
+  <div class="gumroad-product-embed" data-gumroad-product-id="demo" ${embedProduct}><a href="https://gumroad.com/l/demo">Loading...</a></div>`;
 
   const handleChange = (event) => {
     setProduct(event.target.value);
@@ -42,6 +47,10 @@ const Widget = () => {
     setSingleProduct(
       singleProduct === "" ? "data-gumroad-single-product='true'" : ""
     );
+  };
+
+  const toggleEmbedProduct = () => {
+    setEmbedProduct(embedProduct === "" ? "data-outbound-embed='true'" : "");
   };
 
   const overlayTitle = (
@@ -73,8 +82,8 @@ const Widget = () => {
     >
       <span className="fa fa-list-alt fa-fw"></span>
       <ImEmbed2 size="5em" />
-      <h5>Embed</h5>
-      <p>Inline purchases without leaving your site</p>
+      <h5 style={{ color: "#a2a2a2" }}>Embed</h5>
+      <p style={{ color: "#a2a2a2" }}> Embedded products on your site</p>
     </div>
   );
 
@@ -138,9 +147,9 @@ const Widget = () => {
                 buyer confidence.
               </RecommendedText>
               <CodeBox>
-                <OverlayCode placeholder={code} readonly="readonly" />
+                <OverlayCode placeholder={overlayCode} readonly="readonly" />
                 <CopyToClipboard
-                  text={code}
+                  text={overlayCode}
                   onCopy={() => setCopy({ copy: true })}
                 >
                   <BsClipboard
@@ -165,7 +174,54 @@ const Widget = () => {
         <Tab eventKey="embed" title={embedTitle}>
           <Tab.Content>
             <div class="demo border" style={{ height: 400 }}>
-              <p>Embed Content Here</p>
+              <div
+                class="form-check"
+                style={{ margin: "15px 20px", color: "#a2a2a2" }}
+              >
+                <input
+                  class="form-check-input"
+                  value={embedProduct}
+                  type="checkbox"
+                  id="flexCheckAuto"
+                  onClick={toggleEmbedProduct}
+                />
+                <label class="form-check-label" for="flexCheckAuto">
+                  Redirect to Gumroad (recommended to maximize conversions for
+                  non-HTTPS websites)
+                </label>
+              </div>
+              <ClipboardContainer>
+                <p>Loading...</p>
+                <DirectionsText>
+                  Copy and paste this code into your website
+                </DirectionsText>
+                <RecommendedText>
+                  We highly recommend you have an SSL certificate to increase
+                  buyer confidence.
+                </RecommendedText>
+                <CodeBox>
+                  <OverlayCode placeholder={embedCode} readonly="readonly" />
+                  <CopyToClipboard
+                    text={embedCode}
+                    onCopy={() => setCopy({ copy: true })}
+                  >
+                    <BsClipboard
+                      size="2em"
+                      style={{
+                        marginTop: "auto",
+                        marginBottom: "auto",
+                        marginRight: "10px",
+                        color: "turquoise",
+                      }}
+                    />
+                  </CopyToClipboard>
+                  {copy ? (
+                    <span style={{ color: "tomato", margin: "5px 5px" }}>
+                      Copied!
+                    </span>
+                  ) : null}
+                </CodeBox>
+              </ClipboardContainer>
             </div>
           </Tab.Content>
         </Tab>
